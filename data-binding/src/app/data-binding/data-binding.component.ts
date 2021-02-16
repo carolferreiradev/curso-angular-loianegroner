abd34642f03cbe1b2b182622b994c9789a704338
Ana Carolina Ferreira <souzacarol451@gmail.com>
2021-02-16 15:24:20 -0300

    Bindings de evento

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -8,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class DataBindingComponent implements OnInit {
 
   url = 'http://angular.io/';
-  urlImagem = 'https://picsum.photos/400/200'
+  urlImagem = 'https://picsum.photos/400/200';
+  valorAtual = ''
+  valorSalvo: string
+  isMouseOver = false
   getValor() {
     return 1
   }
@@ -17,6 +20,21 @@ export class DataBindingComponent implements OnInit {
 
   getCurtirCurso() {
     return true
+  }
+  botaoClicado() {
+    alert()
+  }
+  onKeyUp(evento: KeyboardEvent) {
+    let teclaPressionada = (<HTMLInputElement>evento.target).value
+    this.valorAtual = teclaPressionada
+  }
+  salvarValor(valor) {
+    this.valorSalvo = valor
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver
+    // negando o valor
   }
 
   constructor() { }
