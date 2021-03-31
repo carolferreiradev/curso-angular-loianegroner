b275ebb5f7bf866ffc4ca1723c10ddc6c18d87b9
Ana Carolina Ferreira <souzacarol451@gmail.com>
2021-03-31 17:55:15 -0300

    Mensagens de erro ao usuario +  Verificar dados no template com JSON

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    DataFormComponent
+    DataFormComponent,
   ],
   imports: [
     BrowserModule,
--- forms/src/app/template-form/template-form.module.ts
+++ forms/src/app/template-form/template-form.module.ts
@@ -1,3 +1,4 @@
+import { FormDebugComponent } from './../form-debug/form-debug.component';
 import { TemplateFormComponent } from './template-form.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -6,6 +7,7 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     TemplateFormComponent,
+    FormDebugComponent
   ],
   imports: [
     CommonModule,
